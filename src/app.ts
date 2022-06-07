99c6c338193ecceb6ad44b09d06e3a468c05d9f5
Wagner Taiatella <wtaiatella@gmail.com>
2022-06-07 18:21:45 -0400

    feat: first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/categories', (req: Request, res: Response) => {
+	res.json([
+		{
+			id: 1,
+			name: 'Jogos',
+		},
+	]);
+});
+
+export default app;
---
+++ tests/integration/categories.spec.ts
@@ -0,0 +1,22 @@
+import app from '@src/app';
+// test
+
+import request from 'supertest';
+
+describe('Categories', () => {
+	describe('GET /categories', () => {
+		it('returns a list of categories in JSON format', () => {
+			const expectedJson = JSON.stringify([
+				{
+					id: 1,
+					name: 'Jogos',
+				},
+			]);
+
+			return request(app)
+				.get('/categories')
+				.expect(200)
+				.expect(expectedJson);
+		});
+	});
+});

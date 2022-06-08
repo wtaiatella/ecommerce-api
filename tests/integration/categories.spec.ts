847dc2dc4cca4075a7c6a0cc622095fcdd3d7569
Wagner Taiatella <wtaiatella@gmail.com>
2022-06-07 20:08:39 -0400

    feat: prettier (#2)
    * feat: prettier
    
    * chore: reviewed files

--- tests/integration/categories.spec.ts
+++ tests/integration/categories.spec.ts
@@ -1,22 +1,19 @@
-import app from '@src/app';
+import app from "@src/app";
 // test
 
-import request from 'supertest';
+import request from "supertest";
 
-describe('Categories', () => {
-	describe('GET /categories', () => {
-		it('returns a list of categories in JSON format', () => {
-			const expectedJson = JSON.stringify([
-				{
-					id: 1,
-					name: 'Jogos',
-				},
-			]);
+describe("Categories", () => {
+  describe("GET /categories", () => {
+    it("returns a list of categories in JSON format", () => {
+      const expectedJson = JSON.stringify([
+        {
+          id: 1,
+          name: "Jogos",
+        },
+      ]);
 
-			return request(app)
-				.get('/categories')
-				.expect(200)
-				.expect(expectedJson);
-		});
-	});
+      return request(app).get("/categories").expect(200).expect(expectedJson);
+    });
+  });
 });

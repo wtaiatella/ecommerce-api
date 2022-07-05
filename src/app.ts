6764193ac83ac29b51b4d5adb0088a338830f4fc
Wagner Taiatella <wtaiatella@gmail.com>
2022-07-05 18:41:08 -0400

    chore: add commit

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,47 @@
-import "dotenv/config";
+import 'dotenv/config';
 
-import express from "express";
-import { Request, Response } from "express";
+import express from 'express';
+import { Request, Response } from 'express';
 
-import logger from "./adapters/logger";
+import logger from './adapters/logger';
 
-import { find } from "@src/services/categoryService";
-import productService from "@src/services/productService";
+import { find } from '@src/services/categoryService';
+
+import categoryService from '@src/services/categoryService';
+import productService from '@src/services/productService';
 
 console.log(process.env.AWARI);
 const app = express();
 
-app.get("/categories", async (req: Request, res: Response) => {
-  const categories = await find();
-  logger.info(`aqui estão as categorias: ${categories}`);
-  res.json(categories);
+app.get('/categories', async (req: Request, res: Response) => {
+	const categories = await find();
+	logger.info(`aqui estão as categorias: ${categories}`);
+	res.json(categories);
 });
 
-app.get("/categories/:id/products", async (req: Request, res: Response) => {
-  const categoryId = req.params.id;
-  logger.debug(`categoryId = ${categoryId}`);
-  const products = await productService.find(categoryId);
-  res.json(products);
+app.get('/categories/:id', async (req: Request, res: Response) => {
+	const categoryId = req.params.id;
+
+	const category = await categoryService.findOne(categoryId);
+
+	res.json(category);
 });
 
-app.get("/products/:id", async (req: Request, res: Response) => {
-  const productId = req.params.id;
-  logger.info({ productId });
+app.get('/categories/:id/products', async (req: Request, res: Response) => {
+	const categoryId = req.params.id;
+	logger.debug(`categoryId = ${categoryId}`);
+	const products = await productService.find(categoryId);
+	res.json(products);
+});
 
-  const product = await productService.findOne(productId);
-  logger.info({ product });
+app.get('/products/:id', async (req: Request, res: Response) => {
+	const productId = req.params.id;
+	logger.info({ productId });
 
-  res.json(product);
+	const product = await productService.findOne(productId);
+	logger.info({ product });
+
+	res.json(product);
 });
 
 export default app;
